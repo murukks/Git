98745532c91e37364a5e928f21788ffe7b042fe5
murukks <murugeshrsm810@gmail.com>
2021-09-09 23:16:53 +0530

    Bworks

--- src/org/git/GitPackage.java
+++ src/org/git/GitPackage.java
@@ -1,6 +1,14 @@
 package org.git;
 
 public class GitPackage {
+	private void getback() {
+		System.out.println("get back up");
+
+	}
+	private void standup() {
+		System.out.println("stand up");
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
