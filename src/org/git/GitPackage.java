fc8051fe6a8df0697ee1df91890e06f613a196f4
murukks <murugeshrsm810@gmail.com>
2021-09-09 23:12:12 +0530

    A works

--- src/org/git/GitPackage.java
+++ src/org/git/GitPackage.java
@@ -1,6 +1,14 @@
 package org.git;
 
 public class GitPackage {
+	private void sample() {
+		System.out.println("sample");
+
+	}
+	private void sample1() {
+		System.out.println("sample2");
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
